706e0e739ecf6b1588406c8220d7833d83994aa0
Thiem Huynh <thiem.huynh0@gmail.com>
2023-10-31 17:59:11 -0400

    file read good

--- 1-fs/fs.js
+++ 1-fs/fs.js
@@ -1,6 +1,19 @@
-const fs = ('fs')
+import fs from 'fs'
+// const process = require('process');
 
-if(process.argv.length === 0){
-    console.log(`Usage: node fs.js [read | create | update | destroy]`)
-    return;
-}
+
+// console.log(process.argv)
+
+// if(process.argv.length === 0){
+//     console.log(`Usage: node fs.js [read | create | update | destroy]`)
+    
+// } else {
+//     console.log('else')
+// }
+
+fs.readFile(`../pets.json`, 'utf8', (error, data) => {
+    if(error){
+        console.error(`Error reading file`, error)
+    }
+    console.log(`file contents:`, data)
+})
