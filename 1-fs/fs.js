75e9eb2114bb779c20ddea72953deb250d4c2262
Patino5 <106916823+Patino5@users.noreply.github.com>
2023-11-01 09:55:14 +1000

    added create f(x), error handling

--- 1-fs/fs.js
+++ 1-fs/fs.js
@@ -1,11 +1,13 @@
 import fs from 'fs';
 import process from 'process';
 
+let petData;
 
 // console.log(process.argv.length)
 
 if(process.argv.length <= 2){
     console.log(`Usage: node fs.js [read | create | update | destroy]`)
+    process.exit(1)
 } else {
     const dbCommand = process.argv[2]
     switch(dbCommand){
@@ -14,7 +16,9 @@ if(process.argv.length <= 2){
             showPets(index);
             break;
         case 'create':
-            console.log(`create`);
+            const args = process.argv.slice(3)
+            const [age, kind, name] = args
+            createPet(age, kind, name)
             break;
         case 'update':
             console.log(`update`);
@@ -24,6 +28,7 @@ if(process.argv.length <= 2){
             break;
         default:
             console.error(`invalid command, check arguments`)
+            process.exit(1)
     }
 }
 
@@ -36,8 +41,44 @@ function showPets(index){
             console.error(`Error reading file`, error)
             process.exit(9);
         }
+        petData = JSON.parse(data)
+        checkRange(index)
         console.log(`file contents:`, JSON.parse(data)[index])
     })
 }
 
+function checkRange(index){
+    if (0 > index || petData.length <= index){
+        console.error(`Usage: node fs.js read INDEX`)//Usage: node fs.js read INDEX
+        process.exit(9);
+    } 
+}
 
+function createPet(arg1, arg2, arg3){
+    const dbPath = `../pets.json`
+    if(arg1 === undefined || arg2 === undefined || arg3 === undefined) {
+        console.log(`Usage: node fs.js create AGE KIND NAME`);
+        process.exit(9)
+    }
+    fs.readFile(dbPath, 'utf8', (error, data) => {
+        if(error){
+            console.error(`Error reading file`, error)
+            process.exit(9);
+        }
+        petData = JSON.parse(data)
+        petData[petData.length] = {
+            age: Number(arg1),
+            kind: arg2,
+            name: arg3
+        }
+
+        console.log(petData);
+        fs.writeFile(dbPath, JSON.stringify(petData), (error) => {
+            if(error){
+                console.error(`Usage: node fs.js read INDEX`)//Usage: node fs.js read INDEX
+                process.exit(9);
+            }
+            console.log('file write success');
+        })
+    })
+}
