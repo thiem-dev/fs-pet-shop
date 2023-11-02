2387364ca7fbf43b9f4d9bdf9ce11b7aa244b18d
Thiem Huynh <thiem.huynh0@gmail.com>
2023-11-02 12:47:12 -0400

    testing express hello world

---
+++ 3-express/express.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import fs from 'fs';
+
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
